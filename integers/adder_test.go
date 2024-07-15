ff7b39496200497304cf52e0145927552b330693
Nikita Pyslar <esyho.main@gmail.com>
2024-07-15 12:19:25 +0300

    go fundamentals: Integers

---
+++ integers/adder_test.go
@@ -0,0 +1,14 @@
+package integers
+
+import (
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", sum, expected)
+	}
+}
